e606d156b784f513b8096ffd0d44f972c72ebccf
Megh <23bmiit146@gmail.com>
2025-12-16 21:50:16 +0530

    Solved the SDK and huge applicationId error

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,10 +3,11 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.sign_flow_1"
+    namespace = "com.kairo.ai"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
